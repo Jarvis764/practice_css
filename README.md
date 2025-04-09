05426f9022a5545b9f4f135030de92666cbdb175
Shreesha <shreesha.alevoor664@gmail.com>
2025-04-09 08:26:29 +0530

    reseted files

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a css minor project in which i have done cards and applied css properties ,made with &hearts;
---
+++ app.js
@@ -0,0 +1,9 @@
+// A simple JavaScript program
+
+// Function to display a greeting message
+function greet(name) {
+    console.log(`Hello, ${name}! Welcome to JavaScript.`);
+}
+
+// Call the function with a name
+greet("Shreesha");
